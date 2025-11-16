1f61ca5d4264a1fe82f5c3489f3087e01671023d
Gimenez Rafael <rafaelgimenez358@gmail.com>
2025-11-16 18:31:54 -0300

    Implementacion de HU-04: Asignación de Servicios a Cuentas

--- facturacion-servicios/src/main/java/com/poo2/facturacion_servicios/controladores/CuentaController.java
+++ facturacion-servicios/src/main/java/com/poo2/facturacion_servicios/controladores/CuentaController.java
@@ -1,8 +1,22 @@
 package com.poo2.facturacion_servicios.controladores;
 
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.poo2.facturacion_servicios.controladores.dto.AsignarServicioDTO;
 import com.poo2.facturacion_servicios.enums.EstadoCuenta;
 import com.poo2.facturacion_servicios.modelos.Cuenta;
+import com.poo2.facturacion_servicios.modelos.ServicioContratado;
 import com.poo2.facturacion_servicios.servicios.CuentaService;
 
 @RestController
@@ -20,5 +34,31 @@ public class CuentaController {
             @PathVariable Long id,
             @RequestParam EstadoCuenta nuevoEstado) {
         return service.cambiarEstadoCuenta(id, nuevoEstado);
+    }
+
+    //Asignar un servicio 
+    @PostMapping("/{cuentaId}/servicios")
+    public ResponseEntity<?> asignarServicio(
+            @PathVariable Long cuentaId,
+            @RequestBody AsignarServicioDTO dto) {
+        
+        ServicioContratado sc = service.asignarServicio(cuentaId, dto.getServicioId());
+        return ResponseEntity.ok(sc);
+    }
+
+    //Quitar un servicio:
+    @DeleteMapping("/{cuentaId}/servicios/{servicioId}")
+    public ResponseEntity<?> quitarServicio(
+            @PathVariable Long cuentaId,
+            @PathVariable Long servicioId) {
+
+        service.quitarServicio(cuentaId, servicioId);
+        return ResponseEntity.noContent().build();
+    }
+
+    //Mostrar servicios contratados
+    @GetMapping("/{cuentaId}/servicios")
+    public List<ServicioContratado> listarServicios(@PathVariable Long cuentaId) {
+        return service.listarServicios(cuentaId);
     }
 }
---
+++ facturacion-servicios/src/main/java/com/poo2/facturacion_servicios/controladores/dto/AsignarServicioDTO.java
@@ -0,0 +1,8 @@
+package com.poo2.facturacion_servicios.controladores.dto;
+
+import lombok.Data;
+
+@Data
+public class AsignarServicioDTO {
+    private Long servicioId;
+}
